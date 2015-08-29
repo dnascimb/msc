4fac9883a149c32efe4b7f2e1fe401cae1b91b1a
Dan Nascimbeni <dnascimb@gmail.com>
2015-08-29 15:16:12 -0700

    disabled autoincrement on service_requests table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 drop table if exists service_requests;
 create table service_requests (
-  id text primary key autoincrement,
+  id text primary key,
   'number' integer not null,
   'type' integer not null,
   'status' text not null,
