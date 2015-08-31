8b4a2209d5d7f304c5f41a11b4fa71c482a1ae0a
Dan Nascimbeni <dnascimb@gmail.com>
2015-08-30 19:31:57 -0700

    added new_service_request screen

--- msc.py
+++ msc.py
@@ -10,6 +10,8 @@
 """
 
 import os
+import uuid
+from datetime import datetime
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
@@ -103,3 +105,70 @@ def home():
 #    cur = db.execute('select uuid, number, type, status, updated_at, provider from service_requests order by number desc')
 #    entries = cur.fetchall()
     return render_template('service_request_listing.html')
+
+
+@app.route('/new_service_request', methods=['GET'])
+def new_service_request():
+    if not session.get('logged_in'):
+        abort(401)
+    return render_template('new_service_request.html')
+
+
+@app.route('/create_service_request', methods=['POST'])
+def create_service_request():
+    if not session.get('logged_in'):
+        abort(401)
+
+    error = None
+    if not validServiceRequest(request):
+        error = 'Invalid data entered'
+        return render_template('new_service_request.html', error=error)
+
+    saveRequest(request)
+    return redirect(url_for('home'))
+
+
+def validServiceRequest(request):
+    # TODO
+    return True
+
+
+def saveRequest(request):
+    warranty = request.form.getlist('checkboxes')
+    #print("warranty: " + warranty)
+    vendor = ''
+    print("vendor: " + vendor)
+    troubleshoot = ''
+    print("troubleshoot: " + troubleshoot)
+    contact_name = request.form['inputContactName']
+    print("contact_name: " + contact_name)
+    contact_phone = request.form['inputContactPhone']
+    print("contact_phone: " + contact_phone)
+    contact_email = request.form['inputContactEmail']
+    print("contact_email: " + contact_email)
+    manufacturer = request.form['inputEquipmentManufacturer']
+    print("manufacturer: " + manufacturer)
+    model = request.form['inputEquipmentModel']
+    print("model: " + model)
+    date_purchased = request.form['inputEquipmentDatePurchased']
+    print("date_purchased: " + date_purchased)
+    appointment = request.form['inputTimeframe']
+    print("appointment: " + appointment)
+    description = request.form['inputDescription']
+    print("description: " + description)
+
+    db = get_db()
+    i = str(uuid.uuid4())
+    number = 3830238
+    rtype = 'Service'
+    status = 'Open'
+    updated_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    provider = 'Advantage'
+
+    #db.execute('insert into service_requests (id, number, type, status, updated_at, provider) values (?, ?)',
+    #           [i, number, rtype, status, udpated_at, provider])
+    #db.commit()
+    flash('New request was successfully submitted')
+
+    return True
+
